70d1007f1f7941798f0e9f1f790601ed042e8288
Mauro Sala <mauro.sala@gmail.com>
2024-04-26 22:30:55 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react'
+import Logo from './assets/logo.svg'
+import Icon from './assets/icon.svg'
+
+import css from './App.module.scss'
+
+import stealth from '@stltio/stealth'
 
 function App() {
+  const [data, setData] = useState({ local: {}, remote: {}, id: '' })
+
+  const init = useCallback(async () => {
+    const s = await stealth()
+    setData(s)
+  }, [])
+
+  useEffect(() => {
+    init()
+  }, [init])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={css.Layout}>
+      <div className={css.Container}>
+        <img src={Logo} alt='Stlt.io' className={css.Logo} />
+
+        <div className={css.Stealth}>
+          <span>Your ID:</span>
+          <code>{data.id}</code>
+          <span>Local information:</span>
+          <code>{JSON.stringify(data.local, null, 2)}</code>
+          <span>Remote information:</span>
+          <code>{JSON.stringify(data.remote, null, 2)}</code>
+        </div>
+
+        <div className={css.Footer}>
+          &copy; {new Date().getFullYear()} Stlt.io - All Rights Reserved
+          <br />
+          <img src={Icon} alt='Stlt.io' />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
